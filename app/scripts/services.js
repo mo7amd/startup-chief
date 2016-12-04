e7dfb467af4600629d9bd1f976e89ad06be628f9
Mohamed.k.Khalil <mo7amd.khaled@gmail.com>
2016-12-04 19:11:11 +0200

    edit controller of the page

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -2,33 +2,33 @@
 
 angular.module('IndexOfApp')
 
-.constant('baseURL','http://localhost:3000/')
+.constant('baseURL', 'https://mo7amd-startup-chief-v1.p.mashape.com/')
 
 .service('IndexFactory', ['$resource', 'baseURL',
-  function($resource,baseURL) {
-    this.getHellow = function() {
-      return $resource(baseURL + 'hellow/', null, {
-          'update': {
-              method: 'PUT'
-          }
-      });
-    };
-  }
-])
-.service('feedbackFactory',['$resource','baseURL',
-function($resource,baseURL) {
-  this.savefeedback = function() {
-    return $resource(baseURL+'contact/',null);
-  };
-}
-])
+        function($resource, baseURL) {
+            this.getHellow = function() {
+                return $resource(baseURL + 'hellow/', null, {
+                    'update': {
+                        method: 'PUT'
+                    }
+                });
+            };
+        }
+    ])
+    .service('feedbackFactory', ['$resource', 'baseURL',
+        function($resource, baseURL) {
+            this.savefeedback = function() {
+                return $resource(baseURL + 'contact/', null);
+            };
+        }
+    ])
 
-.service('subscribeFactory',['$resource','baseURL',
-function($resource,baseURL) {
-  this.saveSubscribe = function() {
-    return $resource(baseURL+'subscribe/',null);
-  };
-}
+.service('subscribeFactory', ['$resource', 'baseURL',
+    function($resource, baseURL) {
+        this.saveSubscribe = function() {
+            return $resource(baseURL + 'subscribe/', null);
+        };
+    }
 ])
 
 ;
