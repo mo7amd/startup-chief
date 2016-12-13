df773ded3bc4af244468f0f9ea0b76fe731a90eb
Mohamed.k.Khalil <mo7amd.khaled@gmail.com>
2016-12-14 01:12:12 +0200

    prepared platform to intgrate with firebase

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,26 +1,86 @@
 'use strict';
 
-angular.module('IndexOfApp', ['ui.router', 'ngResource'])
-    .config(function($stateProvider, $urlRouterProvider) {
-        $stateProvider
+angular.module('IndexOfApp', ['firebase'])
 
-        // route for the home page
-            .state('app', {
-            url: '/',
-            views: {
-                'header': {
-                    templateUrl: 'views/header.html',
-                },
-                'content': {
-                    templateUrl: 'views/home.html',
-                    controller: 'IndexController'
-                },
-                'footer': {
-                    templateUrl: 'views/footer.html',
-                }
-            }
+.controller('IndexController', ['$scope', '$location', '$anchorScroll',
+        function($scope, $location, $anchorScroll) {
 
-        });
+            $scope.showHellow = false;
+            $scope.message = 'Loading ...';
+            $scope.isNavCollapsed = true;
 
-        $urlRouterProvider.otherwise('/');
-    });
+            // *************************************************************************
+            $scope.gotoContact = function() {
+                // console.log('goto contact');
+                $location.hash('contact');
+                $anchorScroll();
+            };
+            $scope.gotoAbout = function() {
+                // console.log('goto about');
+                $location.hash('about');
+                $anchorScroll();
+            };
+
+        }
+    ])
+    .controller('FeedbackController', ['$scope',
+        function($scope) {
+
+            // $scope.sendFeedback = function() {
+
+            //     // console.log($scope.feedback);
+
+            //     if (($scope.feedback.agree) && ($scope.feedback.mychannel === '')) {
+            //         $scope.invalidChannelSelection = true;
+            //         // console.log('incorrect');
+            //     } else {
+            //         feedbackFactory.savefeedback().save($scope.feedback);
+            //         $scope.invalidChannelSelection = false;
+            //         $scope.feedback = {
+            //             mychannel: '',
+            //             firstName: '',
+            //             lastName: '',
+            //             agree: false,
+            //             email: ''
+            //         };
+            //         $scope.feedback.mychannel = '';
+            //         $scope.feedbackForm.$setPristine();
+            // console.log($scope.feedback);
+            //     }
+            // };
+        }
+    ])
+
+.controller('subscribeController', ['$scope',
+    function($scope) {
+
+        //     $scope.sendSubscribe = function() {
+
+        //         // console.log($scope.subscribe);
+
+        //         if (($scope.subscribe.agree) && ($scope.subscribe.mychannel === '')) {
+        //             $scope.invalidChannelSelection = true;
+        //             // console.log('incorrect');
+        //         } else {
+        //             subscribeFactory.saveSubscribe().save($scope.subscribe);
+        //             $scope.invalidChannelSelection = false;
+        //             $scope.subscribe = {
+        //                 email: '',
+        //                 choice: {
+        //                     Marketing: false,
+        //                     Business: false,
+        //                     Financial: false
+        //                 },
+        //                 other: ''
+        //             };
+        //             $scope.subscribeForm.$setPristine();
+        //             // console.log($scope.subscribe);
+        //             $scope.showSubscribe = true;
+        //             $scope.toggleSubscribe = function() {
+        //                 $scope.showSubscribe = !$scope.showSubscribe;
+        //                 // console.log('here');
+        //             };
+        //         }
+        //     };
+    }
+]);
